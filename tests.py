c1bc2ad1f252352dc78110c60ac3070ac625d76d
Eugene Prikazchikov <eprikazc@gmail.com>
2012-06-22 18:52:00 +0400

    Added tests for EpubPage.parse_sections

---
+++ tests.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+from epub import EpubPage
+
+class PageSectionTest(TestCase):
+    def test_top_level_heading(self):
+        in_html = open("test_data/page_sections/in1.html").read()
+        page = EpubPage(None, None, None, in_html, None, None)
+        self.assertEqual(len(page.sections), 1)
+        self.assertEqual(page.sections[0].title, "Git Basics")
+        nested_sections = page.sections[0].children_sections
+        self.assertEqual(5, len(nested_sections))
+        self.assertEqual(nested_sections[0].title, "Snapshots, Not Differences")
+        self.assertEqual(nested_sections[1].title, "Nearly Every Operation Is Local")
+        self.assertEqual(nested_sections[2].title, "Git Has Integrity")
+        self.assertEqual(nested_sections[3].title, "Git Generally Only Adds Data")
+        self.assertEqual(nested_sections[4].title, "The Three States")
+
+    def test_multiple_headings(self):
+        in_html = open("test_data/page_sections/in2.html").read()
+        page = EpubPage(None, None, None, in_html, None, None)
+        self.assertEqual(len(page.sections), 6)
+        self.assertEqual(page.sections[0].title, "Git Basics")
+        self.assertEqual(page.sections[1].title, "Snapshots, Not Differences")
+        self.assertEqual(page.sections[2].title, "Nearly Every Operation Is Local")
+        self.assertEqual(page.sections[3].title, "Git Has Integrity")
+        self.assertEqual(page.sections[4].title, "Git Generally Only Adds Data")
+        self.assertEqual(page.sections[5].title, "The Three States")
+
+    def test_no_first_heading(self):
+        in_html = open("test_data/page_sections/in3.html").read()
+        page = EpubPage(None, None, None, in_html, None, None)
+        self.assertEqual(len(page.sections), 6)
+        self.assertEqual(page.sections[0].title, None)
+        self.assertEqual(page.sections[1].title, "Snapshots, Not Differences")
+        self.assertEqual(page.sections[2].title, "Nearly Every Operation Is Local")
+        self.assertEqual(page.sections[3].title, "Git Has Integrity")
+        self.assertEqual(page.sections[4].title, "Git Generally Only Adds Data")
+        self.assertEqual(page.sections[5].title, "The Three States")
